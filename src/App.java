7e4ed4b4b714a470e8c7449d579327b197171653
seeker <populpock2@gmail.com>
2022-05-06 17:35:56 +0300

    Backup-ANother

--- src/App.java
+++ src/App.java
@@ -1,16 +1,16 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        FormattedInput a = new FormattedInput();
-        System.out.println("\nInput: ");
-        Object[] arr = a.scanf("%s %c %s %d %f %c %d");
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        // FormattedInput a = new FormattedInput();
+        // System.out.println("\nInput: ");
+        // Object[] arr = a.scanf("%s %c %d %f");
 
-        StringBuilder str = new StringBuilder();
-        str.append("Recieved Input: ");
-        for(Object element : arr)
-            str.append(element + " ");
-        System.out.println(str);
+        // StringBuilder str = new StringBuilder();
+        // str.append("Recieved Input: ");
+        // for(Object element : arr)
+        //     str.append(element + " ");
+        // System.out.println(str);
 
         EncodingConverter converterObject = new EncodingConverter();
         converterObject.encodingConverter(args[0], args[1], args[2], args[3]);
